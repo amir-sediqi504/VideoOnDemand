4a5808ee5692061d7594fb6b1449f26d1ce4572a
amir sediqi <amir.sediqi69@gmail.com>
2023-08-28 12:47:49 +0200

    entity classes added

---
+++ VOD.Membership.Database/Entities/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace VOD.Membership.Database.Entities
+{
+    public class Course : IEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(80), Required]
+        public string Title { get; set; }
+        [MaxLength(255)]
+        public string ImageUrl { get; set; }
+        [MaxLength(255)]
+        public string MarqueeImageUrl { get; set; }
+        [MaxLength(1024)]
+        public string Description { get; set; }
+        public bool Free { get; set; }
+
+        public int InstructorId { get; set; }
+        public virtual Instructor Instructor { get; set; }
+        public virtual ICollection<Section> Sections { get; set; }
+    }
+}
