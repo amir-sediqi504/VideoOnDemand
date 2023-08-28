8abe432c57764c627aba6197004758137707dfea
amir sediqi <amir.sediqi69@gmail.com>
2023-08-28 13:04:17 +0200

    context

---
+++ VOD.Common/DTOs/CourseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Common.DTOs
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string MarqueeImageUrl { get; set; }
+        public string Description { get; set; }
+        public bool Free { get; set; }
+
+        public int InstructorId { get; set; }
+        public virtual InstructorDTO Instructor { get; set; } = new();
+        public virtual List<SectionDTO> Sections { get; set; } = new();
+    }
+}
---
+++ VOD.Common/DTOs/SectionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Common.DTOs
+{
+    public class SectionDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int CourseId { get; set; }
+        public string Course { get; set; }
+        public virtual List<VideoDTO> Videos { get; set; }
+    }
+}
